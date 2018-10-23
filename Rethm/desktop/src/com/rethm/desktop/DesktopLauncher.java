44d4ecae8b497704872df3943687c4a576bdd8fc
DESKTOP-ASRock <gregalbelt@gmail.com>
2018-10-24 01:00:16 +0900

    libgdx起動まで再構成

--- Rethm/desktop/src/com/rethm/desktop/DesktopLauncher.java
+++ Rethm/desktop/src/com/rethm/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.zarudama.desktop;
+package com.rethm.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.zarudama.LibgdxSample;
+import com.rethm.Rethm;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LibgdxSample(), config);
+		new LwjglApplication(new Rethm(), config);
 	}
 }
